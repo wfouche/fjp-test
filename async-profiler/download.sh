bbfb1cfae54ff95ca9c608249e318d4594e42513
Werner Fouché <werner.fouche@gmail.com>
2025-11-24 12:20:36 +0200

    Flamegraphs

---
+++ async-profiler/download.sh
@@ -0,0 +1,3 @@
+wget https://github.com/async-profiler/async-profiler/releases/download/v4.2.1/async-profiler-4.2.1-linux-x64.tar.gz
+tar xfvz async-profiler-4.2.1-linux-x64.tar.gz
+rm       async-profiler-4.2.1-linux-x64.tar.gz
---
+++ async-profiler/profile.sh
@@ -0,0 +1,1 @@
+./async-profiler-4.2.1-linux-x64/bin/asprof -d 30 -f flamegraph.html $1  # $1 = PID
