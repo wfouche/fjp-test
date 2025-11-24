b3cd0c13fd20b3e8ce7387fa68d241f2d1fbbc74
Werner Fouché <werner.fouche@gmail.com>
2025-11-24 09:12:21 +0200

    Benchmark run scripts

---
+++ io/tulip/AppJava25.java
@@ -0,0 +1,20 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS io.github.wfouche.tulip:tulip-runtime:2.1.13
+//DEPS org.slf4j:slf4j-api:2.0.17
+//DEPS ch.qos.logback:logback-core:1.5.21
+//DEPS ch.qos.logback:logback-classic:1.5.21
+//SOURCES ForkJoinPoolUser.java
+//JAVA 25
+//RUNTIME_OPTIONS -server -Xms2g -Xmx2g -XX:+UseG1GC
+//FILES ../../benchmark_config.json
+//FILES ../../logback.xml
+
+package io.tulip;
+
+import io.github.wfouche.tulip.api.TulipApi;
+
+public class AppJava25 {
+   public static void main(String[] args) {
+      TulipApi.runTulip("benchmark_config.json");
+   }
+}
