8dfc6928c89f55dd65e21783e5f45df2a2609156
Werner Fouché <werner.fouche@gmail.com>
2025-12-28 15:39:24 +0200

    Updated benchmark reports

--- run_bench_all.sh
+++ run_bench_all.sh
@@ -1,6 +1,8 @@
+rm -f -r benchmark_report* benchmark_output.json benchmark_config.adoc benchmark_config.html
 ./run_bench_17.sh
 ./run_bench_21.sh
 ./run_bench_23.sh
 ./run_bench_24.sh
 ./run_bench_25.sh
 ./run_bench_26.sh
+rm -f -r benchmark_report* benchmark_output.json benchmark_config.adoc benchmark_config.html
