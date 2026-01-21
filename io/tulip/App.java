35ba36e86e37c69e57616e618c22745942aaaab7
Werner Fouché <werner.fouche@gmail.com>
2026-01-21 21:05:14 +0200

    Upgrade to Tulip 2.1.16

--- io/tulip/App.java
+++ io/tulip/App.java
@@ -1,8 +1,8 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.github.wfouche.tulip:tulip-runtime:2.1.15
+//DEPS io.github.wfouche.tulip:tulip-runtime:2.1.16
 //DEPS org.slf4j:slf4j-api:2.0.17
-//DEPS ch.qos.logback:logback-core:1.5.21
-//DEPS ch.qos.logback:logback-classic:1.5.21
+//DEPS ch.qos.logback:logback-core:1.5.25
+//DEPS ch.qos.logback:logback-classic:1.5.25
 //SOURCES ForkJoinPoolUser.java
 //RUNTIME_OPTIONS -Xms2g -Xmx2g -XX:+UseG1GC
 //FILES ../../benchmark_config.json
