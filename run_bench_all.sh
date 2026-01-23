dc94151bf424a2d0d3caaf6a4d0e04eff20af525
Werner Fouché <werner.fouche@gmail.com>
2026-01-23 22:30:24 +0200

    Update Java 25 and Java 26

--- run_bench_all.sh
+++ run_bench_all.sh
@@ -25,13 +25,12 @@ sdk use     java 24.0.2-tem
 
 ./run_bench_24.sh
 
-sdk install java 25.0.1-tem   
-sdk use     java 25.0.1-tem   
-
+sdk install java 25.0.2-tem
+sdk use     java 25.0.2-tem
 ./run_bench_25.sh
 
-sdk install java 26.ea.29-open 
-sdk use     java 26.ea.29-open 
+sdk install java 26.ea.31-open
+sdk use     java 26.ea.31-open
 
 ./run_bench_26.sh
 
