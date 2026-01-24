8cc1f1ea0754ccac9d30e4fcf4e47d9f87533933
Werner Fouché <werner.fouche@gmail.com>
2026-01-24 10:14:47 +0200

    Updated run_bench_all.sh script to install non-default JDKs

--- run_bench_all.sh
+++ run_bench_all.sh
@@ -10,37 +10,37 @@ export SDKMAN_DIR="$HOME/.sdkman"
 rm -f -r app.log benchmark_report* benchmark_output.json benchmark_config.adoc benchmark_config.html
 
 set_title "Java 17"
-sdk install java 17.0.17-tem 
-sdk use     java 17.0.17-tem 
+sdk install java 17.0.18-tem --no-default
+sdk use     java 17.0.18-tem 
 
 ./run_bench_17.sh
 
 set_title "Java 21"
-sdk install java 21.0.9-tem
+sdk install java 21.0.9-tem --no-default
 sdk use     java 21.0.9-tem
 
 ./run_bench_21.sh
 
 set_title "Java 23"
-sdk install java 23.0.2-tem  
+sdk install java 23.0.2-tem --no-default
 sdk use     java 23.0.2-tem  
 
 ./run_bench_23.sh
 
 set_title "Java 24"
-sdk install java 24.0.2-tem 
+sdk install java 24.0.2-tem --no-default
 sdk use     java 24.0.2-tem 
 
 ./run_bench_24.sh
 
 set_title "Java 25"
-sdk install java 25.0.2-tem
+sdk install java 25.0.2-tem --no-default
 sdk use     java 25.0.2-tem
 
 ./run_bench_25.sh
 
 set_title "Java 26"
-sdk install java 26.ea.31-open
+sdk install java 26.ea.31-open --no-default
 sdk use     java 26.ea.31-open
 
 ./run_bench_26.sh
